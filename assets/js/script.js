d1c90e13196e1fbdd7db22d17eb3c32c9307edd4
Gift <56089461+e-giftz@users.noreply.github.com>
2021-03-20 05:11:34 -0400

    Final Clean up of css, js and html files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,15 @@ var timeLeft = localStorage.getItem("timeLeft");
 var containerQuizEl = document.getElementById("container-quiz");
 var questionEl = document.querySelector("#quizquestions");
 var answerBtnEl = Array.from(document.querySelectorAll(".option-answer"));
-var highScoreEl = document.getElementById("finalscore");
-//var highScore = localStorage.getItem("highscore");
 var progressText = document.querySelector("#progressText");
-var scoreText = document.querySelector("#score");
+var scoreText = document.querySelector("#finalscore");
 
-
-// var nextButton = document.createElement("button");
-// nextButton.innerHTML = "Next";
-// containerQuizEl.appendChild(nextButton);
+//var userInitialsEl = document.querySelector("#userid");
+var userInitialsEl = document.getElementById("userid");
+var submitScoreBtnEl = document.querySelector("#submit-form");
+var finalScoreEl = document.querySelector("#container-highScores");
+var mostRecentScoreEl = localStorage.getItem("mostRecentScore");
+var userList = document.querySelector("#user-list");
 
 var currentQuestion = {};
 var scorePoints = 20;
@@ -23,100 +23,31 @@ var getAnswers = true;
 var score = 0;
 var questionCounter = 0;
 var applicableQuestions = [];
+//var users = [];
+var users = Array.from(localStorage.getItem("users"));
 
 startButton.addEventListener("click", startGame);
-
 // Add function to start game
 function startGame(){
+    console.log("hello");
     questionCounter =0;
     score = 0;
-    applicableQuestions = [...questions];
     timeLeft = 75;
+    applicableQuestions = [...questions];
+
     startButton.classList.add('hide-container-quiz');
     containerQuizEl.classList.remove('hide-container-quiz');
     if (containerInfoEl ===  "none") {
         containerInfoEl.style.display = "block";
     } 
     else {
-        containerInfoEl.style.display = "none";
+       containerInfoEl.style.display = "none";
     }
-    
     timerCountdown();
     setNewQuestion();
 }
-
-// Create function for next question
-function setNewQuestion() {
-    //resetQuiz();
-    if (applicableQuestions.length === 0 || questionCounter > totalQuestions) {
-        localStorage.setItem("mostRecentScore", score);
-
-        return;
-    }
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter} of ${totalQuestions}`;
-
-    // Set questions to be displayed randomly
-    var currentQuestionIndex = Math.floor(Math.random() * applicableQuestions.length);
-    currentQuestion = applicableQuestions[currentQuestionIndex];
-    questionEl.innerText =currentQuestion.question;
-
-    answerBtnEl.forEach(option => {
-        var number = option.dataset['number'];
-        //var answerButton = document.createElement("button");
-
-        option.innerText = currentQuestion['option' + number];
-        
-    })
-    applicableQuestions.splice(currentQuestionIndex, 1);
-    getAnswers = true;   
-}
-
-answerBtnEl.forEach(option => {
-    option.addEventListener("click", event => {
-        if (!getAnswers)
-        return
-        getAnswers = false;
-        var selectedEl = event.target;
-        event.stopPropagation();
-        var correctAnswer = selectedEl.dataset['number'];
-
-        var setClass = correctAnswer == currentQuestion.answer ? "correct" : "wrong";
-        if (setClass === 'correct') {
-            incrementScore(scorePoints);
-        }
-        //selectedEl.classList.add("correct");
-        //     event.currentTarget.setAttribute(
-        //         "style",
-        //         "background-color: green"
-        //     );
-    
-        // if ("correct") {
-        //     incrementScore(scorePoints);
-        //     event.currentTarget.setAttribute(
-        //         "style",
-        //         "background-color: green"
-        //     );
-        // }
-        selectedEl.parentElement.classList.add(setClass);
-
-        setTimeout(() => {
-            selectedEl.parentElement.classList.remove(setClass);
-            setNewQuestion();
-        }, 1000)
-    })
-    
-    
-})
-incrementScore = num => {
-    score +=num;
-    scoreText.innerText = score;
-}
-
-
 // Timer that counts down from 75
 function timerCountdown() {
-    
     
     // Method to call timer function
     var timeInterval = setInterval(function () {
@@ -125,7 +56,6 @@ function timerCountdown() {
         
         if (timeLeft >= 0){
             if (getAnswers && timeLeft > 0) {
-                clearInterval(timeInterval);
             }
         }
         // Check if time is run out
@@ -136,6 +66,76 @@ function timerCountdown() {
     }, 1000);
 }
 
+//storeUserInitials(users);
+    
+// function storeUserInitials(data) {
+//     // Stringify and set key in localStorage to todos array
+//     localStorage.setItem("users", JSON.stringify(data));
+//     console.log('users');
+// }
+
+//Get the high score from the local storage
+  var highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+
+
+// Variable to store the maximum numnber of score to record
+var maxHighScore = 5; 
+finalScoreEl.innerText = mostRecentScoreEl;
+
+// Create keyup event to re-enable the submit button when key pressed
+userInitialsEl.addEventListener('keyup', ()=> {     
+    submitScoreBtnEl.disabled = !userInitialsEl.value;
+})
+
+submitScoreBtnEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    //alert("testing")
+    // var userInitialsText = userInitialsEl.value.trim();
+
+    // //return if submitted is blank
+    // if (userInitialsText ==="") {
+    //    return;
+    // }
+
+    // users.push(userInitialsText);
+    // userInitialsEl.value ="";
+    var score = {
+       score: mostRecentScoreEl,
+       name: userInitialsEl.value
+    }
+    
+
+    highScore.push(score)
+
+    highScore.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScore.splice(5);
+
+    localStorage.setItem('users', users);
+    localStorage.setItem('highScore', JSON.stringify(highScore));
+    userList.innerHTML = highScore.map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`
+    }).join('')
+    
+})
+
+// renderUserInitials();
+// function renderUserInitials() {
+
+//     // for (var i = 0; i < userScore.length; i++){
+//     //     var node = document.createElement("p");
+//     //     node.textContent = users[i] + " " + userScore[i];
+//     //     finalScoreEl.append(node);
+//     //     console.log('happy');
+//     // }
+//     userList.innerHTML = 
+//     highScore.map(score => {
+//         return `<li class="high-score">${score.name} - ${score.score}</li>`
+//     }).join('')
+    
+// }
 // Create a list to store object for questions and answers
 var questions  = [
     {
@@ -166,7 +166,55 @@ var questions  = [
         
     }
 ];
+// Create function for next question
+function setNewQuestion() {
+    if (applicableQuestions.length === 0 || questionCounter > totalQuestions) {
+        localStorage.setItem("mostRecentScore", score);
+        return;
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${totalQuestions}`;
 
+    // Set questions to be displayed randomly
+    var currentQuestionIndex = Math.floor(Math.random() * applicableQuestions.length);
+    currentQuestion = applicableQuestions[currentQuestionIndex];
+    questionEl.innerText =currentQuestion.question;
+
+    answerBtnEl.forEach(option => {
+        var number = option.dataset['number'];
+        option.innerText = currentQuestion['option' + number]; 
+    })
+    applicableQuestions.splice(currentQuestionIndex, 1);
+    
+    getAnswers = true;   
+}
+answerBtnEl.forEach(option => {
+    option.addEventListener("click", event => {
+        if (!getAnswers) return
+        getAnswers = false;
+        var selectedEl = event.target;
+        event.stopPropagation();
+        var correctAnswer = selectedEl.dataset['number'];
+
+        var setClass = correctAnswer == currentQuestion.answer ? "correct" : "wrong";
+        if (setClass === 'correct') {
+            incrementScore(scorePoints);
+        }
+
+        selectedEl.parentElement.classList.add(setClass);
+
+        setTimeout(() => {
+            selectedEl.parentElement.classList.remove(setClass);
+            setNewQuestion();
+        }, 1000)
+    })
+    
+})
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 
   
 
